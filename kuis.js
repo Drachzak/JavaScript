83e7524c0a795ae1c5d2fc545caa52bd8cba4f0e
Drachzak <dedet.emang.ok@gmail.com>
2022-01-13 10:19:45 +0700

    first commit

---
+++ kuis.js
@@ -0,0 +1,23 @@
+let firstname = "Dzakwan"
+console.log(firstname)
+let Lastname = "Rachman"
+console.log(Lastname)
+let umur = 15
+console.log(umur)
+const b = "belum menikah"
+const s = "sudah menikah"
+
+let benar = b
+console.log(benar)
+let sudah = s
+//console.log(sudah)
+
+let Restaurant = {
+    name: "Burger Pak Dedet",
+    city: "Yogyakarta",
+    fvdrink: "Mocha Float",
+    favFood: "Burger With Mozarella",
+    isVegan: false
+}
+console.log(Restaurant)
+
